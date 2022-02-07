f62a5bc6568be1f28996584f5402b7d8800234f0
ibratabian17 <ibratabian@gmail.com>
2022-02-07 08:31:59 +0000

    Add Unhud Beat

--- UnHudBeat.js
+++ UnHudBeat.js
@@ -1,38 +1,15 @@
 // ==UserScript==
-// @name         Just Dance Now Anu Core
-// @namespace    JDNAnuCore
-// @version      0.0.1
-// @description  Just Dance Now Anu Core
-// @author       ibratabian17
+// @name         Unhud-Beat
+// @namespace    https://github.com/ibratabian17/JDNaCore
+// @version      0.1.12
+// @description  Add Transition In When Playing JDN!
+// @author       You
 // @match        https://justdancenow.com/
 // @icon         https://www.google.com/s2/favicons?domain=justdancenow.com
 // @grant        none
 // ==/UserScript==
-console.log("Running Patcher..")
 
-//PB Start
 setInterval(function(){
-  var checkExistClass = $(".js").hasClass("state-dance");
-  if(checkExistClass ){
-  var top_bar = $('.fill')
-  var beat = $('#beat')
-  var bg = top_bar.css('background-color')
-  var bga = beat.css('background-color')
-  var color = top_bar.css('color')
-  beat.siblings().css({
-    backgroundColor: bg,
-    color: color
-  })
-  if(bga != color){
-  console.log("Running PictoBar Patcher.." + color)
-  document.getElementById("beat").style.backgroundColor = color;
-  }
-      }
-  }, 500);//run this thang every .5 seconds
-
-
-  //UnHud-Beat
-  setInterval(function(){
     var checkTutorialClass = $(".js").hasClass("state-tutorial");
     if(checkTutorialClass ){
      const el = document.querySelector('.hud');
